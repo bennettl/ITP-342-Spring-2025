f1c2a0212d9be07de850669430193b317162903c
Bennett Lee <bennettlee908@gmail.com>
2025-03-26 19:33:50 -0700

    .

--- Week9/Person.playground/Contents.swift
+++ Week9/Person.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+class Person {
+    let name : String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
+// Class are reference types
+
+let p1 = Person(name: "Amy")
+
+let p2 = Person(name: "John")
+
+let p3 = p1
+
+let p4 = p2
--- Week9/RecapDemo/RecapDemo/ContentView.swift
+++ Week9/RecapDemo/RecapDemo/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct User : Hashable, Identifiable {
+    let id = UUID()
+    let firstName: String
+}
+
+struct UserProfilePage : View {
+    let user : User
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("User profile")
+            Text("This is for user \(user.firstName)")
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+
+    @State var yOffset = 0.0
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // Embeding Destination Views
+                NavigationLink("Go to another page") {
+                    Color.red
+                }
+                NavigationLink("User 1", value: User(firstName: "John"))
+
+                NavigationLink("User 2",  value: User(firstName: "Amy"))
+            }
+            .navigationDestination(for: String.self) { number in
+                Text("Page \(number)")
+            }
+            .navigationDestination(for: User.self) { user in
+                UserProfilePage(user: user)
+            }
+        }
     }
 }
 
